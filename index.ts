c1ff023484791211b7b1718e3bcea8e7031b065d
Bastien MERCIER <method1990@gmail.com>
2017-09-23 20:44:41 +0200

    version 1.0.3, ajout dans les exports du composant test.component.ts, retrait dans la version 1.0.2 du module contenant les services

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { TestService } from './src/app/test/test.service';
-export { TestServiceModule } from './src/app/test.service.module';
-export { TestModule } from './src/app/test.module'; 
+// export { TestServiceModule } from './src/app/test.service.module';
+export { TestModule } from './src/app/test.module';
+export { TestComponent } from './src/app/test/test.component';
